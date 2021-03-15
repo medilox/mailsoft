316d65990953e33a61bb2a5a8061ac8907882f30
Medilox <medilox02@yahoo.fr>
2021-03-15 10:11:40 +0100

    courrier form

--- src/main/resources/static/app/entities/courrier/courrier.controller.js
+++ src/main/resources/static/app/entities/courrier/courrier.controller.js
@@ -5,14 +5,20 @@
         .module('myApp')
         .controller('CourrierController', CourrierController);
 
-    CourrierController.$inject = ['$rootScope', '$state', '$stateParams', 'previousState', 'entity', 'Courrier', 'naturesCourrier'];
+    CourrierController.$inject = ['$rootScope', '$state', '$stateParams', 'previousState', 'entity', 'Courrier', 'naturesCourrier', '$filter'];
 
-    function CourrierController($rootScope, $state, $stateParams, previousState, entity, Courrier, naturesCourrier) {
+    function CourrierController($rootScope, $state, $stateParams, previousState, entity, Courrier, naturesCourrier, $filter) {
         var vm = this;
         vm.courrier = entity;
         vm.previousState = previousState.name;
 
         vm.naturesCourrier = naturesCourrier;
+
+        Courrier.getAll()
+        .then(function(response){
+            vm.courriers = response.data;
+        });
+
 
         vm.save = save;
         function save () {
@@ -31,14 +37,28 @@
         }
 
         function onSaveSuccess (result) {
-            console.log(result)
-            //$scope.$emit('myApp:newCourrier', result);
-            $state.go(vm.previousState);
+            $state.reload();
             vm.isSaving = false;
         }
 
         function onSaveError () {
             vm.isSaving = false;
+        }
+
+        vm.onClickCourrier =  function(courrier){
+            if(vm.courrier.id == courrier.id)
+            vm.courrier = {};
+            else{
+                vm.courrier.id = courrier.id;
+                vm.courrier.refCourrier = courrier.refCourrier;
+                vm.courrier.numCourrier = courrier.numCourrier;
+                vm.courrier.initiateur = courrier.initiateur;
+                vm.courrier.objet = courrier.objet;
+                vm.courrier.natureCourrier =  $filter('filter')(vm.naturesCourrier, {value: courrier.natureCourrier})[0].name;
+                vm.dateEnvoi = moment(courrier.dateEnvoi, 'YYYY-MM-DD').toDate();
+                vm.dateReception = moment(courrier.dateReception, 'YYYY-MM-DD').toDate();
+                vm.courrier.concernes = courrier.concernes;
+            }
         }
     }
 })();
